8023f89f53c5b88f347bcd1877b07a654f3f0ded
memsevimli <43397449+memsevimli@users.noreply.github.com>
2018-09-21 18:56:47 +1000

    user_upload added basic validations

--- user_upload.php
+++ user_upload.php
@@ -2,14 +2,26 @@
 
     $csvFile = 'users.csv';
 
-    $userFile = fopen($csvFile, 'r');
+    $userFile = fopen($csvFile, 'r') or die("Can't open file.");
     
     while (!feof($userFile)) {
         $userArray[] = fgetcsv($userFile, 1024);
     }
 
-    fclose($userFile);
+    fclose($userFile) or die("Can't close file.");
 
-    print_r($userArray);
+    foreach ($userArray as $user) {
+
+        $firstName = ucwords(strtolower(trim($user[0])));
+        $lastName = ucwords(strtolower(trim($user[1])));
+        $email = strtolower(trim($user[2]));
+        
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email = "Invalid Email Format";
+        }
+
+        fwrite(STDOUT, $firstName . " " . $lastName . " " . $email . "\n");
+        
+    }
 
 ?>
