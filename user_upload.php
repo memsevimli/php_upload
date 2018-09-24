e1ed2cbae9a4fd7a9bbec1131620e0b6e2da52d9
memsevimli <memsevimli@mail.com>
2018-09-24 21:54:37 +1000

    Added basic database insert functionality

--- user_upload.php
+++ user_upload.php
@@ -1,8 +1,16 @@
 <?php 
 
+    $db_host = "localhost"; 
+    $db_name = "mysql";
+    $user = "username";
+    $pass = "password";
+
+    // Database connection
+    $connection = mysqli_connect($db_host, $user, $pass, $db_name);
+
     $csvFile = 'users.csv';
 
-    $userFile = fopen($csvFile, 'r') or die("Can't open file.");
+    $userFile = fopen($csvFile, 'r') or die("Can't open csv file.");
     
     while (!feof($userFile)) {
         $userArray[] = fgetcsv($userFile, 1024);
@@ -15,12 +23,19 @@
         $firstName = ucwords(strtolower(trim($user[0])));
         $lastName = ucwords(strtolower(trim($user[1])));
         $email = strtolower(trim($user[2]));
-        
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $email = "Invalid Email Format";
-        }
 
-        fwrite(STDOUT, $firstName . " " . $lastName . " " . $email . "\n");
+        $firstName = mysqli_real_escape_string($connection, $firstName);
+        $lastName = mysqli_real_escape_string($connection, $lastName);
+        $email = mysqli_real_escape_string($connection, $email);
+
+        $query = "INSERT INTO users (name, surname, email) VALUES ('$firstName', '$lastName', '$email')";
+        
+        if (mysqli_query($connection, $query)) {
+            echo "New record added";
+        }
+        else {
+            echo mysqli_error($connection);
+        }
         
     }
 
